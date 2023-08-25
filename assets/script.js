15af9a61deebe6084093e162bd77e6daa39d1f80
Brock Williams <b.williams54255@gmail.com>
2023-08-24 22:52:14 -0700

    Added to function in the javacsript that listens for the save button and applies logic for the time blocks. Applies the class of past present or future depnding on the current hour based off dayjs.hour.

---
+++ assets/script.js
@@ -0,0 +1,21 @@
+$(function () {
+  // 1. Save button listener
+  $(".saveBtn").click(function () {
+    let hour = $(this).parent().attr("id");
+    let userInput = $(this).siblings("textarea").val();
+    localStorage.setItem(hour, userInput);
+  });
+
+  // 2. Apply past, present, future class
+  $(".time-block").each(function () {
+    let blockHour = parseInt($(this).attr("id").split("-")[1]);
+    let currentHour = dayjs().hour();
+
+    if (blockHour < currentHour) {
+        $(this).addClass("past");
+    } else if (blockHour === currentHour) {
+        $(this).addClass("present");
+    } else {
+        $(this).addClass("future");
+    }
+  });
